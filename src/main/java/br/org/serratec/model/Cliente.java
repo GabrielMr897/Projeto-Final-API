5cbd746f64df439a8097b87b5257573710ffa7b8
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-17 18:14:21 -0300

    criando colunas das classes model

--- src/main/java/br/org/serratec/model/Cliente.java
+++ src/main/java/br/org/serratec/model/Cliente.java
@@ -1,5 +1,96 @@
 package br.org.serratec.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.GenerationType;
+
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long idCliente;
+
+    @Column(name = "nome_usuario")
+    private String nomeUsuario;
+
+    @Column(name = "nome_completo")
+    private String nomeCompleto;
+
+    private String senha;
+
+    @CPF(message = "preencha o cpf corretamente")
+    private String cpf;
+
+    private String telefone;
+
+    @Column(name = "data_nasc")
+    private LocalDate dataNascimento;
+    
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
 }
--- src/main/java/br/org/serratec/model/Produto.java
+++ src/main/java/br/org/serratec/model/Produto.java
@@ -1,5 +1,95 @@
 package br.org.serratec.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.ietf.jgss.Oid;
+
+
+@Entity
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private Long idProduto;
+
+    private String nome;
+
+    private String descricao;
+
+    @Column(name = "qtd_Estoque")
+    private Integer quantidadeEstoque;
+
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    @Column(name = "valor_unitario")
+    private Double valorUnitario;
+
+    private Oid imagem;
+    
+
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Oid getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(Oid imagem) {
+        this.imagem = imagem;
+    }
+
 
 }
