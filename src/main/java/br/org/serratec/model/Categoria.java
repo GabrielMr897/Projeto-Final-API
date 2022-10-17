ecf4f43fb98a40c7bb80d8387dd354a5687d2baf
JotaMarquesb <jonathanmarquesb@hotmail.com>
2022-10-17 16:33:20 -0300

    Classe Categoria

--- src/main/java/br/org/serratec/model/Categoria.java
+++ src/main/java/br/org/serratec/model/Categoria.java
@@ -1,5 +1,20 @@
 package br.org.serratec.model;
 
-public class Categoria {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_categoria")
+	private Long id;
+	private String nome;
+	private String descricao;
+		
+	
 }
