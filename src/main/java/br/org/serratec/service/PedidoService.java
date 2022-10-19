edab22ef324fbbc136670c37e6e7965d2bd58df5
KauanOliveira1 <oliveirakauan7@gmail.com>
2022-10-19 17:02:28 -0300

    Criando service e controller de pedidos

--- src/main/java/br/org/serratec/service/PedidoService.java
+++ src/main/java/br/org/serratec/service/PedidoService.java
@@ -1,5 +1,36 @@
 package br.org.serratec.service;
 
-public class PedidoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.model.Pedido;
+import br.org.serratec.repository.PedidoRepository;
+
+@Service
+public class PedidoService {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    
+    public Pedido inserir(Pedido p) {
+        pedidoRepository.save(p);
+        return p;
+    }
+    
+    public List<Pedido> listar(){
+        return pedidoRepository.findAll();
+    }
+    
+    public Optional<Pedido> buscar(Long id){
+        Optional<Pedido> pedido = pedidoRepository.findById(id);
+        return pedido;
+    }
+    
+    public Pedido update (Pedido pedido, Long id) {
+        pedido.setIdPedido(id);
+        return pedidoRepository.save(pedido);
+    }
 }
+
