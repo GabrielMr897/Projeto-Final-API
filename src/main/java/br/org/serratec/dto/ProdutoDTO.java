f93b42f83743e20b0018a1e07699971030d83453
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-20 23:21:42 -0300

    fix

--- src/main/java/br/org/serratec/dto/ProdutoDTO.java
+++ src/main/java/br/org/serratec/dto/ProdutoDTO.java
@@ -2,26 +2,23 @@ package br.org.serratec.dto;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Optional;
 
 import br.org.serratec.model.Categoria;
 import br.org.serratec.model.Produto;
 
-
-public class ProdutoDTO implements Serializable{
+public class ProdutoDTO implements Serializable {
     private Long idProduto;
     private String nome;
     private String descricao;
     private LocalDate dataCadastro;
     private Double valorUnitario;
     private Categoria categoria;
-    
+
     private String uri;
-    
-    
+
     public ProdutoDTO() {
     }
-    
+
     public ProdutoDTO(Produto produto) {
         this.idProduto = produto.getIdProduto();
         this.nome = produto.getNome();
@@ -30,48 +27,61 @@ public class ProdutoDTO implements Serializable{
         this.valorUnitario = produto.getValorUnitario();
         this.categoria = produto.getCategoria();
     }
+
     public Long getIdProduto() {
         return idProduto;
     }
+
     public void setIdProduto(Long idProduto) {
         this.idProduto = idProduto;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getDescricao() {
         return descricao;
     }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
     public LocalDate getDataCadastro() {
         return dataCadastro;
     }
+
     public void setDataCadastro(LocalDate dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
+
     public Double getValorUnitario() {
         return valorUnitario;
     }
+
     public void setValorUnitario(Double valorUnitario) {
         this.valorUnitario = valorUnitario;
     }
+
     public Categoria getCategoria() {
         return categoria;
     }
+
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
+
     public String getUri() {
         return uri;
     }
+
     public void setUri(String uri) {
         this.uri = uri;
     }
-    
-    
+
 }
