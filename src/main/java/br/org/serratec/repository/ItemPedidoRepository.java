48d7f283d0048c2530ba3b3b4807ff9e349ac1aa
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-21 22:02:18 -0300

    método buscarTotalPedido

--- src/main/java/br/org/serratec/repository/ItemPedidoRepository.java
+++ src/main/java/br/org/serratec/repository/ItemPedidoRepository.java
@@ -1,9 +1,15 @@
 package br.org.serratec.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.org.serratec.model.ItemPedido;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-    
+
+    @Query(nativeQuery = true, value = "select id_pedido, sum(preco_venda) from item_pedido where :idPedido group by id_pedido")
+    public Page<ItemPedido> buscarTotalPedido(Long idPedido, Pageable pageable);
+
 }
