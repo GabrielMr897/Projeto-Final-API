dc8dd4fe007e5a5afbed3fcd49eb1afa58ee17d9
Gabriel Martins <gabriel-pms@hotmail.com>
2022-10-26 13:52:44 -0300

    fix controller

--- src/main/java/br/org/serratec/controller/ClienteController.java
+++ src/main/java/br/org/serratec/controller/ClienteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
--- src/main/java/br/org/serratec/controller/ProdutoController.java
+++ src/main/java/br/org/serratec/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
